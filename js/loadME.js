497af46962d078bba5a8e6257aa25f8c4b49db13
Ryan Bridglal <social@it.ryanjb.com>
2015-04-23 17:47:25 -0400

    Create loadME.js
    create the file

---
+++ js/loadME.js
@@ -0,0 +1,60 @@
+<!-- hide from non JavaScript Browsers
+
+Image1 = new Image(248,1249)
+Image1.src = "http://www.itdevz.com/clients/ryanjb/images/bgPortal.jpg"
+
+Image2 = new Image(736,489)
+Image2.src = "http://www.itdevz.com/clients/ryanjb/images/Myself.jpg"
+
+Image3 = new Image(736,489)
+Image3.src = "http://www.itdevz.com/clients/ryanjb/images/Portfolio.jpg"
+
+Image4 = new Image(736,489)
+Image4.src = "http://www.itdevz.com/clients/ryanjb/images/Definitions_Portfolio.jpg"
+
+Image5 = new Image(736,489)
+Image5.src = "http://www.itdevz.com/clients/ryanjb/images/News.jpg"
+
+Image6 = new Image(736,489)
+Image6.src = "http://www.itdevz.com/clients/ryanjb/images/Blog.jpg"
+
+Image7 = new Image(736,489)
+Image7.src = "http://www.itdevz.com/clients/ryanjb/images/Shop.jpg"
+
+Image8 = new Image(240,240)
+Image8.src = "http://www.itdevz.com/clients/ryanjb/images/img_geo.jpg"
+
+Image9 = new Image(240,240)
+Image9.src = "http://www.itdevz.com/clients/ryanjb/images/img_wel.jpg"
+
+Image10 = new Image(240,240)
+Image10.src = "http://www.ryanjb.com/html5/images/img_work01.jpg"
+
+Image11 = new Image(240,240)
+Image11.src = "http://www.ryanjb.com/html5/images/img_work.jpg"
+
+Image12 = new Image(240,240)
+Image12.src = "http://www.ryanjb.com/html5/images/img_play02.jpg"
+
+Image13 = new Image(240,240)
+Image13.src = "http://www.ryanjb.com/html5/images/img_play.jpg"
+
+Image14 = new Image(240,240)
+Image14.src = "http://www.ryanjb.com/html5/images/img_news03.jpg"
+
+Image15 = new Image(240,240)
+Image15.src = "http://www.ryanjb.com/html5/images/img_news.jpg"
+
+Image16 = new Image(240,240)
+Image16.src = "http://www.ryanjb.com/html5/images/img_blog04.jpg"
+
+Image17 = new Image(240,240)
+Image17.src = "http://www.ryanjb.com/html5/images/img_blog.jpg"
+
+Image18 = new Image(240,240)
+Image18.src = "http://www.ryanjb.com/html5/images/img_shop05.jpg"
+
+Image19 = new Image(240,240)
+Image19.src = "http://www.ryanjb.com/html5/images/img_shop.jpg"
+
+// End Hiding -->
